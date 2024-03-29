fb3ef74b3897463aee30364e5fc4e6102fb02ce4
unknown <nahimathalake@gmail.com>
2024-03-29 01:07:54 +0100

    tu menerve

--- pratique.py
+++ pratique.py
@@ -1,6 +1,6 @@
 import streamlit as st
-import pandas as pd
-import seaborn as sns
+#import pandas as pd
+#import seaborn as sns
 
 def main():
     st.title('Application WIADA 2023')
@@ -15,30 +15,7 @@ def main():
         else:
             st.write('Bonjour à tous !')
     
-    # Importer des données fictives pour l'exemple
-    data = pd.DataFrame({
-        'Pays': ['France', 'Allemagne', 'Espagne', 'Italie', 'Royaume-Uni'],
-        'Population (millions)': [67, 83, 47, 60, 68]
-    })
     
-    # Afficher les données
-    st.subheader('Données démographiques')
-    st.write(data)
-    
-    # Afficher un graphique interactif avec seaborn
-    st.subheader('Graphique de la population par pays')
-    sns.barplot(x='Pays', y='Population (millions)', data=data)
-    st.pyplot()
-    
-    # Bouton pour télécharger les données
-    if st.button("Télécharger les données au format CSV"):
-        csv = data.to_csv(index=False)
-        st.download_button(
-            label="Télécharger",
-            data=csv,
-            file_name='donnees_demographiques.csv',
-            mime='text/csv'
-        )
 
 if __name__ == "__main__":
     main()
